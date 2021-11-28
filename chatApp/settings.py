75787bc89b5b04b5160ee7852f6ad11fc274ef92
ChuksMbanaso <89187837+ChuksMbanaso@users.noreply.github.com>
2021-11-28 17:12:22 +0100

    Updated settings

--- chatApp/settings.py
+++ chatApp/settings.py
@@ -144,9 +144,9 @@ INTERNAL_IPS = [
 ]
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
-    'API_KEY': os.environ.get('API_KEY'),
-    'API_SECRET': os.environ.get('API_SECRET'),
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME')
+    'API_KEY': os.environ.get('API_KEY')
+    'API_SECRET': os.environ.get('API_SECRET')
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
